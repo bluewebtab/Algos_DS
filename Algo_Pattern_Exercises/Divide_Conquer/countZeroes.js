d05fc65a29587323fc3303c0d2e1e12b690945ba
Ronny <ronny.icne@gmail.com>
2021-11-09 23:36:16 -0800

    update

--- Algo_Pattern_Exercises/Divide_Conquer/countZeroes.js
+++ Algo_Pattern_Exercises/Divide_Conquer/countZeroes.js
@@ -1,13 +1,23 @@
 function firstZero(arr, low, high){
 
     if(high >= low){
-        var mid = low + parseInt((high - low) / 2);
-        if((mid == 0 || arr[mid - 1] == 1) && arr[mid] == 0)
+        console.log(high)
+        console.log(low)
+        var mid = low + Math.floor((high - low) / 2);
+        console.log(mid)
+        if((mid == 0 || arr[mid - 1] == 1) && arr[mid] == 0){
+            console.log(mid)
             return mid;
-        if(arr[mid] == 1)
+        }
+        if(arr[mid] == 1){
+            console.log(mid + 1, high)
             return firstZero(arr, (mid + 1), high);
-        else    
+        }      
+        else{
+            console.log(low, mid -1 )
             return firstZero(arr, low, (mid - 1))
+        }  
+            
     }
     return - 1;
 }
@@ -15,13 +25,15 @@ function firstZero(arr, low, high){
 function countZeroes(arr){
     var n = arr.length
     var first = firstZero(arr, 0, n - 1)
-
+    console.log(first)
+    console.log(first)
     if (first == -1){
         return 0;
     }
+    console.log(first)
     return (n - first)
 }
 
 
 
-console.log(countZeroes([1,1,0,0, 0, 0, 0]))
+console.log(countZeroes([1,1,1,0,0, 0]))
